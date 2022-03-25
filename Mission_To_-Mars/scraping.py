213d975629967754133fc7ae487a2a755cc2ee4c
josagie17 <josagie17@gmail.com>
2022-03-25 14:38:50 -0400

    Add files via upload

---
+++ Mission_To_-Mars/scraping.py
@@ -0,0 +1,134 @@
+
+# Import Splinter, BeautifulSoup and Pandas
+from splinter import Browser
+from bs4 import BeautifulSoup as soup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import datetime as dt
+
+
+def scrape_all():
+    # set executable path
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now(),
+        "hemispheres": hemisphere_images(browser),
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
+
+
+def mars_news(browser):
+
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
+
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
+
+    # Convert the browser html to a soup object and then quit the browser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
+    # Add try/except for error handling
+    try:
+        slide_element = news_soup.select_one('div.list_text')
+        slide_element.find('div', class_='content_title')
+
+        # use parent element to find the first 'a' tag and save it as "news_title"
+        news_title = slide_element.find(
+            'div', class_='content_title').get_text()
+
+        # use parent element to find paragraph text
+        news_p = slide_element.find(
+            'div', class_='article_teaser_body').get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
+
+ # ## JPL Space Images Featured Image
+
+
+def featured_image(browser):
+
+    # Visit URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
+
+    try:
+        # Find the relative image
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+
+    except AttributeError:
+        return None
+
+    # use base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+
+    return img_url
+
+
+# ### Mars Facts
+def mars_facts():
+    try:
+        # use 'read_html" to scrape the facts table into a dataframe
+        df = pd.read_html('https://galaxyfacts-mars.com/')[0]
+    except BaseException:
+        return None
+    # Assign columns and set index of Dataframe
+    df.columns = ['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+
+    # convert DataFrame back to html
+    return df.to_html()
+
+# ### Hemisphere Scrape
+def hemisphere_images(browser):
+    # 1. Use browser to visit the URL 
+    url = 'https://marshemispheres.com/'
+    browser.visit(url)
+
+    # 2. Create a list to hold the images and titles.
+    hemisphere_image_urls = []
+    links = browser.find_by_css('a.product-item img')
+
+    # 3. Write code to retrieve the image urls and titles for each hemisphere.
+    for i in range(len(links)):
+        browser.find_by_css('a.product-item img')[i].click()
+        hemisphere = {}
+        sample_elem = browser.links.find_by_text('Sample').first
+        hemisphere['img_url'] = sample_elem['href']
+        hemisphere['title'] = browser.find_by_css('h2.title').text
+        hemisphere_image_urls.append(hemisphere)
+        browser.back()
+    
+    return hemisphere_image_urls
+
+    # shutdown
+    browser.quit()
+
+
+if __name__ == "__main__":
+    # If running as script, print scraped data
+    print(scrape_all())
